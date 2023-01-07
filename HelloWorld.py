eceae641ecb85043fbf1ad45bace0184b9c7a13b
Omamaa16 <omamamashhood10@gmail.com>
2023-01-07 19:30:29 +0500

    ignore added

---
+++ HelloWorld.py
@@ -0,0 +1,42 @@
+print('omama')
+
+var = 'Omama Mashhood'
+type(var)
+var.count('a')
+
+MAX_C=6000
+
+Colors = ['red', 'blue', 'green', 'yellow']
+print(Colors)
+print(Colors[0])
+
+Names = ['string', 0, 1, 4, 5, ('age', 20)]
+print(Names)
+print(Names[5])
+
+message = f"hi this is {var}"
+print(message)
+
+for i in range (1,5):
+ print (i)
+
+ for Names in Names:
+    print(f'hello {var}')
+
+for i, name in enumerate(Names):
+    print(f' hello {name} at index: {i}')
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.reverse()
+print(motorcycles)
+
+foods = ['biryani', 'pizza', 'burger']
+my_list= foods.copy()
+print(my_list)
+
+house = ['villa', 'home', 'hut', 'house']
+for house in house:
+    if(car==cars)
+fro names, languages, in favorite_languages.items
+
+musician = 1
